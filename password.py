a81d36bdb9985e036f736fc3ac72b34d354a476d
Gothrait <97575284+Gothrait@users.noreply.github.com>
2022-03-10 03:18:45 -0500

    Create password.py
    experimenting with the while statement. the program does not function.

---
+++ password.py
@@ -0,0 +1,11 @@
+while True:
+    print('What is your name?')
+    name = input()
+    if name != 'Gothrait':
+        continue
+print('What is the password?')
+password = input()
+if password == 'fun':
+        print('Welcome back Gothrait!')
+else:
+        print('You are not welcome here.')
