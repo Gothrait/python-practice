b77a9b7b47e695e439f03f7ad7c6d0ede8a76337
Gothrait <97575284+Gothrait@users.noreply.github.com>
2022-05-20 20:01:25 -0500

    infiniloop.py
    an infinite loop. i get why it infinitely loops but don't grasp how to get it to stop. i'm not sure what i'm not understanding.

---
+++ infiniloop.py
@@ -0,0 +1,14 @@
+# i understand why it is an infinite loop but don't know how to make it stop looping unsure what i am not grasping
+
+def powers(v):
+  while v % 2 < 0:
+    v = v / 2
+  if n == 1:
+    return True
+  return False
+  
+
+print(is_power_of_two(0))
+print(is_power_of_two(1))
+print(is_power_of_two(8))
+print(is_power_of_two(9))
